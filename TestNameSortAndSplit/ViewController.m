6dddc5eab37906b99398ef58c23d2a6c2f75fc46
easyhaipi <easy_qingsong@163.com>
2017-02-16 14:22:33 +0800

    s

--- TestNameSortAndSplit/ViewController.m
+++ TestNameSortAndSplit/ViewController.m
@@ -27,9 +27,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    [self initData];
+       [self initData];
     [self createTableView];
 }
 
